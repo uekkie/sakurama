095e5544637b3d937878509a2b171d9e2139d95d
Hiroshi Ueki <hirocueki@gmail.com>
2018-10-09 17:36:52 +0900

    bootstrap4 へ移行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180916023957) do
+ActiveRecord::Schema.define(version: 2018_09_16_023957) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
