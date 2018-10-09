f0e3372099616c16560c1b86e197f27870b9080a
Hiroshi Ueki <hirocueki@gmail.com>
2018-10-09 18:14:07 +0900

    scaffold product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_16_023957) do
+ActiveRecord::Schema.define(version: 2018_10_09_091040) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.string "image"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
