4dfb14bf07c39b789d1c135fb9a849ebba08294c
Hiroshi Ueki <hirocueki@gmail.com>
2018-10-16 17:33:20 +0900

    implement controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,2 +1,26 @@
 class ChargesController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    # Amount in cents
+    @amount = 500
+
+    cusomer = Stripe::Customer.create(
+      email: params[:stripeEmail],
+      source: params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      customer: customer.id,
+      amount: @amount
+      description: 'うまいさかな',
+      currency: 'jpy'
+    )
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+  end
 end
