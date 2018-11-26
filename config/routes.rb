17a7ffd91366b35a79384186343c981519f0f295
Hiroshi Ueki <hirocueki@gmail.com>
2018-11-26 21:47:53 +0900

    管理者用の商品管理コントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'products/new'
+    get 'products/edit'
+    get 'products/show'
+    get 'products/index'
+  end
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :products
   root 'products#index'
